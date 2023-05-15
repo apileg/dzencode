bda01c35d48505b7f0228a458ade93389eea2c02
apileg <33292686+apileg@users.noreply.github.com>
2023-05-15 20:10:24 +0300

    WIP on _app markup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import Navbar from "@/components/navbar/Navbar"
+import TopPanel from "@/components/top-panel/TopPanel"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <div className="flex h-screen overflow-hidden">
+            <div>
+                <Navbar />
+            </div>
+            <div className="relative"></div>
+            <TopPanel />
+            <Component {...pageProps} />
+        </div>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import Navbar from "@/components/navbar/Navbar"
 import TopPanel from "@/components/top-panel/TopPanel"
 
 export default function Home() {
-    return <TopPanel />
+    return <></>
 }
