c96256fe801bdbdd60432b2530dc0cb95cc3ce37
apileg <33292686+apileg@users.noreply.github.com>
2023-05-17 14:40:36 +0300

    add 404 page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,17 @@ import type { AppProps } from "next/app"
 import Navbar from "@/components/navbar/Navbar"
 import TopPanel from "@/components/top-panel/TopPanel"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { useRouter } from "next/router"
 
 export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter()
     const queryClient = new QueryClient()
+
+    // Don't apply the layout to the 404 page
+    // https://github.com/vercel/next.js/discussions/37311#discussioncomment-2851217
+    if (router.pathname === "/404") {
+        return <Component {...pageProps} />
+    }
 
     return (
         <QueryClientProvider client={queryClient}>
