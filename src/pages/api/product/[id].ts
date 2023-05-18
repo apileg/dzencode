ccbe5ac2f93809e4e70627daf292f110931c2bba
apileg <33292686+apileg@users.noreply.github.com>
2023-05-18 22:43:33 +0300

    fix delete product bug

--- src/pages/api/product/[id].ts
+++ src/pages/api/product/[id].ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/prisma"
 import { NextApiHandler } from "next"
 import { parseId } from "../_parseId"
+import { Prisma } from "@prisma/client"
 
 const handler: NextApiHandler = async (req, res) => {
     try {
@@ -16,11 +17,28 @@ const handler: NextApiHandler = async (req, res) => {
             return
         }
 
-        await prisma.productEntity.delete({ where: { id } })
-        res.status(200)
+        try {
+            await prisma.productEntity.delete({ where: { id } })
+            res.status(200)
+        } catch (error) {
+            if (isDeleteNonExistentError(error)) {
+                res.status(404)
+                return
+            }
+
+            res.status(500)
+            return
+        }
     } finally {
         res.end()
     }
+}
+
+function isDeleteNonExistentError(error: any) {
+    return (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2025"
+    )
 }
 
 export default handler
